60006b0b46d3a19bf07fa1382c0407b922c8c68a
norman2001 <normanraschke@m-ruoll01-wlan.mobile>
2017-03-09 15:16:17 +0100

    Enabled New Row Handling
    A new function handles the submit event.
    The values of the input fields are used to create a new row.
    This row will be appended to the existing rows.
    After this the input field will be cleared and the cursor will jump to the first field.

--- js/einkaufsliste.js
+++ js/einkaufsliste.js
@@ -1,23 +1,25 @@
 document.querySelector('#artikelForm').addEventListener('keyup', toggleButton)
 document.querySelector('#artikelForm').addEventListener('change', toggleButton)
+document.querySelector('#artikelForm').addEventListener('submit', handleFormSubmit)
 
 function toggleButton (e) {
   var form = this
   var allFieldsNotEmpty = true
   var button = form.querySelector('#input-absenden')
 
-  // Alle Eingabefelder selectieren
+  // Select all input fields
   var inputArtikel = form.querySelector('.artikel-eingeben')
   var selectEinheit = form.querySelector('#einheit-auswaehlen')
-  var inputMenge = form.querySelector('.menge-eingeben')
+  var inputMenge = form.querySelector('#menge-eingeben')
 
-  // Werte der Felder auslesen
+  // Read values from fields
   var values = []
   values.push(inputArtikel.value)
-  values.push(selectEinheit.children[selectEinheit.selectedIndex].value)
+  var einheit = selectEinheit.children[selectEinheit.selectedIndex].value
+  values.push(einheit)
   values.push(inputMenge.value)
 
-  // Prüfen ob alle Felder gefüllt sind
+  // Check if all fields are filled
   for (var i = 0; i < values.length; i++) {
     if (values[i].length === 0) {
       allFieldsNotEmpty = false
@@ -25,10 +27,63 @@ function toggleButton (e) {
     }
   }
 
-  // Button aktivieren bzw. deaktivieren
+  // Activate or deactivate button
   if (allFieldsNotEmpty) {
     button.removeAttribute('disabled')
   } else {
     button.setAttribute('disabled', 'disabled')
   }
+}
+
+function createNewRow (artikel, einheit, menge) {
+  // Create new row with values
+  var newRow = document.createElement('div')
+  newRow.classList.add('tabellenzeile')
+
+  var newArtikel = document.createElement('div')
+  newArtikel.classList.add('artikel')
+  newArtikel.textContent = artikel
+  newRow.appendChild(newArtikel)
+
+  var newEinheit = document.createElement('div')
+  newEinheit.classList.add('einheit')
+  newEinheit.textContent = einheit
+  newRow.appendChild(newEinheit)
+
+  var newMenge = document.createElement('div')
+  newMenge.classList.add('menge')
+  newMenge.textContent = menge
+  newRow.appendChild(newMenge)
+
+  return newRow
+}
+
+function handleFormSubmit (e) {
+  // Verhindern des Absenden und Neuladen durch den Browser
+  e.preventDefault()
+  // Container for new table rows
+  var table = document.querySelector('.tabelle')
+
+  // Extracting user data from fields
+  var form = this // Wir sitzen auf dem Form
+  var artikel = form.querySelector('.artikel-eingeben').value
+
+  //  DOM-Node = 'select' (HTML-Code)
+  var selectEinheit = form.querySelector('#einheit-auswaehlen')
+  //  DOM-Nodes = 'options' (HTML-Code)
+  var options = selectEinheit.children
+  //  Currently selected index of 'options'
+  var selectedIndex = selectEinheit.selectedIndex
+  //  Read value from selected option tag
+  var einheit = options[selectedIndex].value
+
+  var menge = form.querySelector('#menge-eingeben').value
+  // Creating new table row (invisible)
+  var newRow = createNewRow(artikel, einheit, menge)
+  // Appending new row to existing container (visible)
+  table.appendChild(newRow)
+  // Clearing input fields
+  form.reset()
+  // Set cursor into first input field
+  form.querySelector('.artikel-eingeben').focus()
 }
