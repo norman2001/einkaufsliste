6eb65872d2c687cf58e0508b3e1d51666d45e146
norman2001 <normanraschke@Martins-MacBook-Pro-2.local>
2017-03-12 20:49:48 +0100

    Added Fuctions for Edilt and Delete Icons
    I have added the functions for the events `edit a row` and `delete a row`.
    By click on `edit` the values are worn-in the input fields and removed from list. After editing the input can be added to the list again.

--- js/einkaufsliste.js
+++ js/einkaufsliste.js
@@ -1,6 +1,7 @@
 document.querySelector('#artikelForm').addEventListener('keyup', toggleButton)
 document.querySelector('#artikelForm').addEventListener('change', toggleButton)
 document.querySelector('#artikelForm').addEventListener('submit', handleFormSubmit)
+document.querySelector('#tableTail').addEventListener('click', handleRowClick)
 
 function toggleButton (e) {
   var form = this
@@ -8,7 +9,7 @@ function toggleButton (e) {
   var button = form.querySelector('#input-absenden')
 
   // Select all input fields
-  var inputArtikel = form.querySelector('.artikel-eingeben')
+  var inputArtikel = form.querySelector('#artikel-eingeben')
   var selectEinheit = form.querySelector('#einheit-auswaehlen')
   var inputMenge = form.querySelector('#menge-eingeben')
 
@@ -38,7 +39,7 @@ function toggleButton (e) {
 function createNewRow (artikel, einheit, menge) {
   // Create new row with values
   var newRow = document.createElement('div')
-  newRow.classList.add('tabellenzeile')
+  newRow.classList.add('row')
 
   var newCheckbox = document.createElement('input')
   newCheckbox.setAttribute('type', 'checkbox')
@@ -60,13 +61,15 @@ function createNewRow (artikel, einheit, menge) {
   newMenge.textContent = menge
   newRow.appendChild(newMenge)
 
-  var newPencil = document.createElement('i')
-  newPencil.classList.add('fa', 'fa-pencil')
-  newRow.appendChild(newPencil)
+  var newEditButton = document.createElement('i')
+  newEditButton.classList.add('fa', 'fa-pencil')
+  newEditButton.setAttribute('data-type', 'edit')
+  newRow.appendChild(newEditButton)
 
-  var newTrash = document.createElement('i')
-  newTrash.classList.add('fa', 'fa-trash')
-  newRow.appendChild(newTrash)
+  var newDeleteButton = document.createElement('i')
+  newDeleteButton.classList.add('fa', 'fa-trash')
+  newDeleteButton.setAttribute('data-type', 'delete')
+  newRow.appendChild(newDeleteButton)
 
   return newRow
 }
@@ -75,19 +78,19 @@ function handleFormSubmit (e) {
   // Verhindern des Absenden und Neuladen durch den Browser
   e.preventDefault()
   // Container for new table rows
-  var table = document.querySelector('.tabelle')
+  var table = document.querySelector('#tableTail')
 
   // Extracting user data from fields
   var form = this // Wir sitzen auf dem Form
-  var artikel = form.querySelector('.artikel-eingeben').value
+  var artikel = form.querySelector('#artikel-eingeben').value
 
-  //  DOM-Node = 'select' (HTML-Code)
+  // DOM-Node = 'select' (HTML-Code)
   var selectEinheit = form.querySelector('#einheit-auswaehlen')
-  //  DOM-Nodes = 'options' (HTML-Code)
+  // DOM-Nodes = 'options' (HTML-Code)
   var options = selectEinheit.children
-  //  Currently selected index of 'options'
+  // Currently selected index of 'options'
   var selectedIndex = selectEinheit.selectedIndex
-  //  Read value from selected option tag
+  // Read value from selected option tag
   var einheit = options[selectedIndex].value
 
   var menge = form.querySelector('#menge-eingeben').value
@@ -99,5 +102,86 @@ function handleFormSubmit (e) {
   // Clearing input fields
   form.reset()
   // Set cursor into first input field
-  form.querySelector('.artikel-eingeben').focus()
+  form.querySelector('#artikel-eingeben').focus()
+}
+
+function handleRowClick (e) {
+  var tableTail = this
+  var button = e.srcElement
+    // Wenns nicht der Klick auf den Button war:
+  if (!button) return
+    // If source element is 'Delete' button (data-type="delete")
+    // https://wiki.selfhtml.org/wiki/Data-Attribut
+  if (button.dataset.type === 'delete') {
+    var row = getRowOfButton(button)
+    if (row) deleteRow(tableTail, row)
+  }
+  // If source element is 'Edit' button
+  if (button.dataset.type === 'edit') {
+    var row = getRowOfButton(button)
+    if (row) {
+      var values = getValuesFromRow(row)
+        // Name für neue Funktion wählen und aufrufen
+      insertValuesIntoForm(values)
+      // Entfernen der Row, damit sie nach dem Editieren nicht doppelt vorhanden ist
+      deleteRow(tableTail, row)
+    }
+  }
+
+  // Else do nothing
+  // möglich wäre 'return' - aber unnötig
+}
+
+function deleteRow (tableTail, row) {
+  tableTail.removeChild(row)
+}
+
+function insertValuesIntoForm (values) {
+  var form = document.querySelector('#artikelForm')
+  var inputArtikel = form.querySelector('#artikel-eingeben')
+  inputArtikel.value = values[0]
+
+  var selectEinheit = form.querySelector('#einheit-auswaehlen')
+  var options = selectEinheit.querySelectorAll('option')
+  var einheit = values[1]
+
+  for (var i = 0; i < options.length; i++) {
+    var optionNode = options[i]
+    var optionValue = optionNode.getAttribute('value')
+      //
+    if (einheit === optionValue) {
+      selectEinheit.selectedIndex = i
+      break
+    }
+  }
+
+  var inputMenge = form.querySelector('#menge-eingeben')
+  inputMenge.value = values[2]
+}
+
+function getValuesFromRow (row) {
+  // Wenn die Row leer ist, steige aus
+  if (row === undefined) return
+
+  // artikelNode  = selektiere DOM-Node '.artikel' (ROW!!!)
+  // Mit document.querySelector gäbe es immer nur die erste Zeile im DOM zurück
+  var artikelNode = row.querySelector('.artikel')
+  var artikel = artikelNode.textContent
+
+  // ODER: (Function Chaining)
+  var einheit = row.querySelector('.einheit').textContent
+
+  var mengeNode = row.querySelector('.menge')
+  var menge = mengeNode.textContent
+
+  // Gib die Werte der Variablen zurück
+  return [artikel, einheit, menge]
+}
+
+function getRowOfButton (button) {
+  if (button.parentNode && button.parentNode.classList.contains('row')) {
+    return button.parentNode
+  } else {
+    return undefined
+  }
 }
